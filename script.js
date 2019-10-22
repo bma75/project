05dc6a9b65ad658b6f3740106c7752abfa1721e9
макс <example@gmail.com>
2019-10-22 07:54:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,132 @@
+'use strict';
+
+//alert("Я JavaScript!");
+
+/*let admin, name;
+name = "Джон";
+admin = name;
+alert ( admin );*/
+
+/*let ourPlanetName = "Земля";
+let let currentUserName = "Джон";*/
+
+/*const birthday = '18.04.1982';
+
+const age = someCode(birthday);
+
+const BIRTHDAY = '18.04.1982'; // использовать заглавные буквы?
+
+const AGE = someCode(BIRTHDAY); // а здесь?*/
+
+
+
+/*var leftBorderWith = 1;
+let second = 2;
+const pi = 3.14;
+console.log(second);*/
+
+/*var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false*/
+//};
+
+//console.log(persone.name);
+/*console.log(persone["name"]);
+
+let arr = ['plum.png' , 'orange.jpg' , 'apple.bmp'];
+
+console.log(arr[0])*/
+
+//alert("Hello World!");
+
+//confirm("Are you here?");
+
+/*let answer = confirm("Are you here?");
+console.log(answer);*/
+
+/*let answer = prompt("Есть ли Вам 18?" , "Да");
+console.log(answer);*/
+
+/*let answer = prompt("Есть ли Вам 18?" , "Да");
+console.log(typeof(answer));*/
+
+/*let answer = prompt("Есть ли Вам 18?" , "Да");
+console.log(typeof(arr));*/
+
+/*let answer = +prompt("Есть ли Вам 18?" , "Да");
+console.log(typeof(null));*/
+
+/*Console.log()
+//var leftBorderWith = 1;
+//let second = 2;
+//const pi = 3.14;
+
+//console.log(4/0);
+//console.log('String'*9);
+//let something;
+//console.log(something);
+
+//let persone = {
+  //  name: "John",
+   // age: 25,
+   // isMarried: false
+//};
+
+//console.log(persone["name"]);
+
+//let arr = ['plum.png' , 'orange.jpg' , 'apple.bmp'];
+
+//console.log(arr[0]);*/
+/*let incr = 10,
+    decr = 10;*/
+
+    /*incr++;
+    decr--;*/
+
+/*console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" == 2); сравнение значений
+console.log("2" === 2); сравнение по типам данных*/
+
+/*let isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose); логическое сравнение*/
+
+/*let isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose);*/
+
+/*let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose) или если один true (false только если оба);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || isClose);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose); ! - знак отрицания превращает в обратное*/
+
